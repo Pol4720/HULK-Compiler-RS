e97704c98aad3a43eff12f722351e55cd0dde2cf
AbrahamRom <127030430+AbrahamRom@users.noreply.github.com>
2025-06-09 14:30:25 -0400

    Implementación de la generación de código para Assignment, incluyendo la búsqueda de puntero en la tabla de símbolos y la emisión de la instrucción de almacenamiento.

--- Compiler/src/hulk_Tokens/hulk_assignment.rs
+++ Compiler/src/hulk_Tokens/hulk_assignment.rs
@@ -1,11 +1,9 @@
 use super::hulk_identifier::Identifier;
+use crate::codegen::context::CodegenContext;
+use crate::codegen::traits::Codegen;
 use crate::hulk_tokens::hulk_expression::Expr;
 use crate::visitor::hulk_accept::Accept;
 use crate::visitor::hulk_visitor::Visitor;
-use crate::codegen::traits::Codegen;
-use crate::codegen::context::CodegenContext;
-
-
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Assignment {
@@ -15,11 +13,12 @@ pub struct Assignment {
 
 impl Assignment {
     pub fn new(identifier: Identifier, expression: Box<Expr>) -> Self {
-        Assignment { identifier, expression }
+        Assignment {
+            identifier,
+            expression,
+        }
     }
 }
-
-
 
 impl Accept for Assignment {
     fn accept<V: Visitor<T>, T>(&self, visitor: &mut V) -> T {
@@ -29,7 +28,19 @@ impl Accept for Assignment {
 
 impl Codegen for Assignment {
     fn codegen(&self, context: &mut CodegenContext) -> String {
-        // TODO: Implement codegen for Assignment
-        String::new()
+        // Busca el puntero de la variable en la tabla de símbolos
+        let var_name = &self.identifier.id;
+        if let Some(ptr) = context.symbol_table.get(var_name) {
+            // Genera el valor de la expresión
+            let value_reg = self.expression.codegen(context);
+            // Emite la instrucción de almacenamiento
+            context.emit(&format!("  store i32 {}, i32* {}", value_reg, ptr));
+            value_reg
+        } else {
+            panic!(
+                "Variable '{}' no definida en el contexto para asignación",
+                var_name
+            );
+        }
     }
 }
